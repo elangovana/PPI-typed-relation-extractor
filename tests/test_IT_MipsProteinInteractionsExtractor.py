682fb23f371b0fd6dc48a1d50ad9ac63ff8e5fc1
elangovana <aparnaelangovan@gmail.com>
2018-06-06 21:42:09 +1000

    fix path

--- tests/test_IT_MipsProteinInteractionsExtractor.py
+++ tests/test_IT_MipsProteinInteractionsExtractor.py
@@ -18,7 +18,7 @@ class TestMipsProteinInteractionsExtractor(unittest.TestCase):
     def test_extract_protein_interactions_kgml(self, mips_file, expected_no_rel):
         #Arrange
         full_mips_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), mips_file)
-        sut = MipsProteinInteractionsExtractor()
+        sut = MipsProteinInteractionsExtractor(full_mips_file_path)
 
         with open(full_mips_file_path, 'r') as myfile:
             #Act
