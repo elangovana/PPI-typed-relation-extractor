13f2779096a4f300da568ce9925f566c8d48e3b4
aeg <aeg@amazon.com>
2018-07-14 14:49:33 +1000

    Add more data processor

--- tests/dataloadertests/test_dataprocessor.py
+++ tests/dataloadertests/test_dataprocessor.py
@@ -6,12 +6,13 @@ from ddt import ddt, data
 
 from dataloader.dataPreprocessor import DataPreprocessor
 
+
 @ddt
 class TestDataPreprocessor(TestCase):
 
     @data("data/human_13_negative.xml")
     def test_Convert_to_json(self, xmlfile):
-        #Arrange
+        # Arrange
         fulXmlFilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), xmlfile)
         fulXsltFilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data/flatten.xslt")
         outhandler = BytesIO()
@@ -19,11 +20,9 @@ class TestDataPreprocessor(TestCase):
 
         with open(fulXmlFilePath, "rb") as xmlHandler:
             with open(fulXsltFilePath, "rb") as xsltHandler:
+                # Act
+                sut.transform(xmlHandler, xsltHandler, outhandler)
 
-                    #Act
-                    sut.transform(xmlHandler, xsltHandler, outhandler )
-
-        #Assert if output has any content
+        # Assert if output has any content
         outhandler.seek(0)
-        self.assertEqual(len(outhandler.read(10)),10)
-
+        self.assertEqual(len(outhandler.read(10)), 10)
