9df08c4e7648026cb9aca297cec0f631b5d282e6
aeg <aeg@amazon.com>
2018-07-14 17:06:13 +1000

    flatten data

--- dataloader/dataPreprocessor.py
+++ dataloader/dataPreprocessor.py
@@ -1,10 +1,9 @@
 import lxml.etree as ET
 import os
-
-
+from io import BytesIO
 class DataPreprocessor:
 
-    def transform(self, xmlHandle, outputHandle):
+    def transform(self, xmlHandle):
         dom = ET.parse(xmlHandle)
         fulXsltFilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "flatten.xslt")
         with (open(fulXsltFilePath, "rb")) as xsltHandle:
@@ -12,5 +11,26 @@ class DataPreprocessor:
             transform = ET.XSLT(xslt)
             newdom = transform(dom)
 
+            outputHandle = BytesIO()
         newdom.write(outputHandle)
-        return outputHandle
+        outputHandle.seek(0)
+        for entry in self._iter_elements_by_name(outputHandle, "data", {}):
+            yield  ET.tostring( entry)
+
+
+
+    def _iter_elements_by_name(self, handle, name, namespace):
+        events = ET.iterparse(handle, events=("start", "end"))
+        _, root = next(events)  # Grab the root element.
+
+        expanded_name = name
+        # If name has the namespace, expand it
+        if ":" in name :
+            local_name = name[name.index(":") + 1:]
+            namespace_short_name = name[:name.index(":")]
+            expanded_name = "{{{}}}{}".format(namespace[namespace_short_name], local_name)
+
+        for event, elem in events:
+            if event == "end" and elem.tag == expanded_name:
+                yield elem
+                elem.clear()
--- tests/dataloadertests/test_dataprocessor.py
+++ tests/dataloadertests/test_dataprocessor.py
@@ -2,7 +2,7 @@ from io import BytesIO
 from unittest import TestCase
 
 import os
-from ddt import ddt, data
+from ddt import ddt, data, unpack
 
 from dataloader.dataPreprocessor import DataPreprocessor
 
@@ -10,18 +10,21 @@ from dataloader.dataPreprocessor import DataPreprocessor
 @ddt
 class TestDataPreprocessor(TestCase):
 
-    @data("data/human_13_negative.xml")
-    def test_Convert_to_json(self, xmlfile):
+    @data(("data/human_13_negative.xml", 7))
+    @unpack
+    def test_Convert_to_json(self, xmlfile, expected_no_interactions):
         # Arrange
         fulXmlFilePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), xmlfile)
 
-        outhandler = BytesIO()
+
         sut = DataPreprocessor()
 
         with open(fulXmlFilePath, "rb") as xmlHandler:
             # Act
-            sut.transform(xmlHandler, outhandler)
+            actual = []
+            for item in sut.transform(xmlHandler):
+                actual.append(item)
 
         # Assert if output has any content
-        outhandler.seek(0)
-        self.assertEqual(len(outhandler.read(10)), 10)
+
+        self.assertEqual(len(actual), expected_no_interactions)
