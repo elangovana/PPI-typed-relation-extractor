dbced90c16156eb7122816dfeda7611365f88133
aeg <aeg@amazon.com>
2018-07-14 10:29:46 +1000

    Add script to download ftp files

---
+++ scripts/dowloadintactinteractions.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#!/bin/sh
+HOST='ftp.ebi.ac.uk'
+USER='anonymous'
+PASSWD='anonymous'
+FILE=/pub/databases/intact/current/psi25/species/human*.xml
+LDIR=./data
+ftp -n $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+prompt noprompt
+lcd $LDIR
+cd /pub/databases/intact/current/psi25/species
+mget human*.xml
+quit
+END_SCRIPT
