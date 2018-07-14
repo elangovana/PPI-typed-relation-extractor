90a83b016af916f48d90afea97ebfb4eeb2f0d4a
aeg <aeg@amazon.com>
2018-07-14 10:49:36 +1000

    Add script to download ftp files

--- scripts/dowloadintactinteractions.sh
+++ scripts/dowloadintactinteractions.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-#!/bin/sh
+#!/bin/bash
 HOST='ftp.ebi.ac.uk'
 USER='anonymous'
 PASSWD='anonymous'
 FILE=/pub/databases/intact/current/psi25/species/human*.xml
+s3destination=$1
+
+
 mkdir data
 LDIR=data
 ftp -n $HOST <<END_SCRIPT
@@ -15,3 +17,11 @@ cd /pub/databases/intact/current/psi25/species
 mget human*.xml
 quit
 END_SCRIPT
+
+if ["$s3destination"!=""];
+then
+    aws s3 copy data/* $s3destination
+else
+    echo "No s3destination argument passed, hence not copying to s3. If you want to copy data to s3 .."
+    echo "$0 s3://mybucket/data"
+fi
