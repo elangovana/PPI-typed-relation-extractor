21445ee9a95476f1e6bb1312c17a0a28ff81aebd
aeg <aeg@amazon.com>
2018-07-30 21:23:51 +1000

    Install FTP

--- scripts/dowloadintactinteractions.sh
+++ scripts/dowloadintactinteractions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 HOST='ftp.ebi.ac.uk'
 USER='anonymous'
 PASSWD='anonymous'
@@ -7,20 +9,20 @@ s3destination=$2
 LDIR=$1
 
 
-ftp -n $HOST <<END_SCRIPT
+ftp -n -v $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 prompt noprompt
 lcd $LDIR
-cd /pub/databases/intact/current/psi25/species
+cd /pub/databases/IntAct/current/psi25/species/
 mget human*.xml
-quit
+
 END_SCRIPT
 
-if ["$s3destination"!=""];
-then
-    aws s3 copy data/* $s3destination
-else
+if ["$s3destination" == ""]; then
     echo "No s3destination argument passed, hence not copying to s3. If you want to copy data to s3 .."
     echo "$0 s3://mybucket/data"
+
+else
+    aws s3 copy data/* $s3destination
 fi
--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 s3destination=$1
 set -e
 mkdir data
@@ -6,7 +6,7 @@ mkdir outdir
 
 #Download ftp files
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-sh $scripts_dir/dowloadintactinteractions.sh data
+. $scripts_dir/dowloadintactinteractions.sh data
 
 #process data
 source_dir=$scripts_dir/..
@@ -14,10 +14,10 @@ python $source_dir/dataloader/bulkImexDataPreprocessor.py data outdir
 
 
 
-if ["$s3destination"!=""];
-then
-    aws s3 copy outdir/* $s3destination
-else
+if ["$s3destination" == ""]; then
     echo "No s3destination argument passed, hence not copying to s3. If you want to copy data to s3 .."
     echo "$0 s3://mybucket/data"
+else
+
+    aws s3 copy outdir/* $s3destination
 fi
