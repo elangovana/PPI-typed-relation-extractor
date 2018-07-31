e0b673bd2e64634c779deb0bf3a381c73fe697bc
aeg <aeg@amazon.com>
2018-07-31 21:13:48 +1000

    Clean up invocation scripts

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-filepattern=$1
-s3destination=$2
+datadir=$1
+filepattern=$2
+s3destination=$3
+processeddatadir=$datadir/processed
 set -e
-mkdir data
-mkdir outdir
+mkdir $processeddatadir
+
 
 #Download ftp files
 echo "Downloading files.."
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-. $scripts_dir/dowloadintactinteractions.sh data $filepattern
+. $scripts_dir/dowloadintactinteractions.sh $datadir $filepattern
 
 #process data
-echo "Processing data.."
+echo "Processing data..in directory $datadir"
 source_dir=$scripts_dir/..
 export PYTHONPATH=$source_dir
-python $source_dir/dataloader/bulkImexDataPreprocessor.py data outdir
+python $source_dir/dataloader/bulkImexDataPreprocessor.py $datadir $processeddatadir
 
 
 
 if ["$s3destination" == ""]; then
     echo "No s3destination argument passed, hence not copying to s3. If you want to copy data to s3 .."
-    echo "$0 s3://mybucket/data"
+    echo "$0  <outdir>  <filepattern> <s3://mydesbucket/data>"
 else
 
-    aws s3 copy outdir/* $s3destination
+    aws s3 copy $processeddatadir/* $s3destination
 fi
