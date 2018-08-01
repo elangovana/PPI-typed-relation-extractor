afa14f8f4f6272d30db56c53b9ad5d2f152cef5b
aeg <aeg@amazon.com>
2018-08-01 22:20:00 +1000

    add logging and copy to s3

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -10,7 +10,7 @@ mkdir $processeddatadir
 #Download ftp files
 echo "Downloading files.."
 scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-. $scripts_dir/dowloadintactinteractions.sh $datadir $filepattern
+bash $scripts_dir/dowloadintactinteractions.sh $datadir $filepattern
 
 #process data
 echo "Processing data..in directory $datadir"
@@ -19,11 +19,11 @@ export PYTHONPATH=$source_dir
 python $source_dir/dataloader/bulkImexDataPreprocessor.py $datadir $processeddatadir
 
 
-
+echo "Attempting to copy to s3 if specified $s3destination"
 if [ "$s3destination" == "" ]; then
     echo "No s3destination argument passed, hence not copying to s3. If you want to copy data to s3 .."
     echo "$0  <outdir>  <filepattern> <s3://mydesbucket/data>"
 else
-
-    aws s3 copy $processeddatadir/* $s3destination
+    echo "Copying from $processeddatadir/* to $s3destination"
+    aws s3 cp $processeddatadir/* $s3destination
 fi
