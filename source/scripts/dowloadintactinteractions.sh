5748118dc902464e198549250507ea9ef9098010
aeg <aeg@amazon.com>
2018-09-23 18:07:26 +1000

    Fix s3 upload ftp

--- source/scripts/dowloadintactinteractions.sh
+++ source/scripts/dowloadintactinteractions.sh
@@ -8,18 +8,24 @@ FTPDIR=/pub/databases/intact/current/psi25/species
 s3destination=$3
 filepattern=$2
 LDIR=$1
+ftpmode=$4
 
 if [ "$filepattern" == "" ]; then
     filepattern="human_01*.xml"
+fi
+
+
+if [ "$ftpmode" == "" ]; then
+    ftpmode="on"
 fi
 
 echo "Downloading files from $FTPDIR to $LDIR, matching pattern $filepattern from host $HOST"
 ftp -n -v $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
-prompt noprompt
 lcd $LDIR
-passive
+passive ${ftpmode}
+prompt off
 cd $FTPDIR
 mget $filepattern
 quit
@@ -27,7 +33,7 @@ END_SCRIPT
 
 if [ "$s3destination" == "" ]; then
     echo "No s3destination argument passed, hence not copying to s3. If you want to copy data to s3 .."
-    echo "$0 <localdir>  <filepatterntodownload> <s3://mydestinationbucket/data>"
+    echo "$0 <localdir>  <filepatterntodownload> <s3://mydestinationbucket/data> <passive mode on|off>"
 
 else
     aws s3 cp ${LDIR}/ $s3destination --recursive --include *.xml
