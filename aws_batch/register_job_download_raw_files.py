b55f0a64f6f20e6c5dff77912650cd5f85d65f12
aeg <aeg@amazon.com>
2018-09-24 20:02:17 +1000

    Fix register raw download job

--- aws_batch/register_job_download_raw_files.py
+++ aws_batch/register_job_download_raw_files.py
@@ -1,5 +1,8 @@
 import argparse
+
 import boto3
+
+from aws_batch.createrole import create_role
 
 
 class RegisterJob:
@@ -9,20 +12,63 @@ class RegisterJob:
         self.account = account or boto3.client('sts').get_caller_identity().get('Account')
         self.region = aws_region or boto3.session.Session().region_name
 
-    def run(self, container_name: str):
+    def run(self, container_name: str, data_bucket:str):
         """
         Registers a job with aws batch.
+        :param data_bucket: the name of the s3 bucket that will hold the data
         :param container_name: The name of the container to use e.g 324346001917.dkr.ecr.us-east-2.amazonaws.com/awscomprehend-sentiment-demo:latest
         """
+        job_def_name = "KeggPathWayExtractor_simplepipeline"
+        role_name = "AWSBatchECSRole_{}".format(job_def_name)
+
+        ##This is mandatory for aws batch
+        assume_role_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "",
+                    "Effect": "Allow",
+                    "Principal": {
+                        "Service": "ecs-tasks.amazonaws.com"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }
+
+        #This is custom for the batch
+        access_policy = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Sid": "BucketAccess",
+                    "Effect": "Allow",
+                    "Action": [
+                        "s3:PutObject",
+                        "s3:GetObject",
+                        "s3:ListBucket",
+                        "s3:DeleteObject",
+                        "s3:HeadBucket"
+                    ],
+                    "Resource": [
+                        "arn:aws:s3:::{}/*".format(data_bucket),
+                        "arn:aws:s3:::{}".format(data_bucket)
+                    ]
+                }
+            ]
+        }
+
+        managed_policy_arns =["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+
+        create_role(role_name, assume_role_policy, access_policy, managed_policy_arns)
 
         job_definition = {
-            "jobDefinitionName": "KeggPathWayExtractor_downloadfile",
+            "jobDefinitionName":job_def_name,
             "type": "container",
             "parameters": {
                 "file_pattern": "human_*.xml",
                 "s3_destination": "s3://<bucker>/prefix/",
-                "local_dir": "/data",
-                "log_level":"--log-level INFO"
+                "local_dir": "/data"
 
             },
             "containerProperties": {
@@ -33,10 +79,9 @@ class RegisterJob:
                     "scripts/dowloadintactinteractions.sh",
                     "Ref::local_dir",
                     "Ref::file_pattern",
-                    "Ref::s3_destination",
-                    "Ref::log_level"
+                    "Ref::s3_destination"
                 ],
-                "jobRoleArn": "arn:aws:iam::{}:role/Batch".format(self.account),
+                "jobRoleArn": "arn:aws:iam::{}:role/{}".format(self.account, role_name),
                 "volumes": [
                     {
                         "host": {
@@ -77,7 +122,10 @@ if __name__ == '__main__':
     parser.add_argument("containerimage",
                         help="Container image, e.g docker pull lanax/kegg-pathway-extractor:latest")
 
+    parser.add_argument("data_bucket",
+                        help="The names of the s3 bucket that will contain the input/output data")
+
     job = RegisterJob()
     args = parser.parse_args()
-    result = job.run(args.containerimage)
+    result = job.run(args.containerimage, args.data_bucket)
     print(result)
