b1fde357f69dbb574f71de0887ddfec508b3cd05
aeg <aeg@amazon.com>
2018-09-26 20:46:53 +1000

    update tests

--- tests/test_dataextractor/test_PubMedAbstractExtractor.py
+++ tests/test_dataextractor/test_PubMedAbstractExtractor.py
@@ -13,13 +13,15 @@ class TestPubmedAbstractExtractor(unittest.TestCase):
     def setUp(self):
         fileConfig(os.path.join(os.path.dirname(__file__), 'logger.ini'))
 
-    @data(("data/sample_pubmed_data.xml", 2))
+    @data(("data/sample_pubmed_data.xml", 2)
+          ,("data/sample_pubmed_data_specialcharacter.xml",1))
     @unpack
-    def test_extract_protein_interactions_kgml(self, mips_file, expected_no_rel):
+    def test_extract(self, pubmed_resp, expected_no_rel):
         #Arrange
-        sut = PubmedAbstractExtractor()
+        sut = PubmedAbstractExtractor(pubmed_baseurl="http://localhost/invalid_mock_shoudl_not_be_is_use")
 
-        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), mips_file), 'r') as myfile:
+
+        with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), pubmed_resp), 'r') as myfile:
             #Act
             actual = sut.extract(myfile)
 
