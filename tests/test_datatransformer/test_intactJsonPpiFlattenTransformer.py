049a42de05812c390843d72d25f37263589bd982
aeg <aeg@amazon.com>
2018-10-30 10:51:51 +1100

    Move from restorer to data transformer

--- tests/test_datatransformer/test_intactJsonPpiFlattenTransformer.py
+++ tests/test_datatransformer/test_intactJsonPpiFlattenTransformer.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import pandas as pd
 
-from datavisualiser.jsonPPIFlattenTransformer import IntactJsonPpiFlattenTransformer
+from datatransformer.jsonPPIFlattenTransformer import IntactJsonPpiFlattenTransformer
 
 
 class TestIntactJsonPpiFlattenTransformer(TestCase):
