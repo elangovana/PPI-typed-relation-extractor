865d763520b1b96c449cfe7189b70bc4df46b557
aeg <aeg@amazon.com>
2018-11-01 18:04:30 +1100

    Fix params

--- source/algorithms/main_train.py
+++ source/algorithms/main_train.py
@@ -18,14 +18,14 @@ def run(data_file, embedding_file, tmp_dir, interaction_type="phosphorylation"):
     labels = data[["isNegative"]]
     data = data[["interactionType", "pubmedabstract", "destAlias", "sourceAlias"]]
 
-    X_train, X_test, X_train, y_test = train_test_split(data, labels, train_size=.8,
+    X_train, X_test, y_train, y_test = train_test_split(data, labels, train_size=.8,
                                                         random_state=777)
 
     # TODO: For the moment just filter one type of classification and do binary as true vs false
     with open(embedding_file, "r") as embedding:
         train_factory = RelationExtractionFactory(embedding, embedding_dim=300, class_size=class_size,
                                                   output_dir=tmp_dir, ngram=1)
-        train_factory(X_train, X_train, X_test, y_test)
+        train_factory(X_train, y_train, X_test, y_test)
 
 
 
@@ -34,13 +34,14 @@ if "__main__" == __name__:
     parser.add_argument("inputjson",
                         help="The input json data")
     parser.add_argument("embedding", help="The embeeing file")
-    parser.add_argument("out-dir", help="The output dir")
+    parser.add_argument("outdir", help="The output dir")
     parser.add_argument("--interaction-type", help="The interction type", default="phosphorylation")
 
     args = parser.parse_args()
 
     run(args.inputjson,args.embedding,
-        args.out_dir, args.interaction_type)
+        args.outdir, args.interaction_type)
+
 
 
 
