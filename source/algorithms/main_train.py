fdc89ea6b78e01b5da4039059d267f5528dbc4b1
aeg <aeg@amazon.com>
2018-11-01 19:48:31 +1100

    add emed dim

--- source/algorithms/main_train.py
+++ source/algorithms/main_train.py
@@ -7,7 +7,7 @@ import logging
 from sklearn.model_selection import train_test_split
 
 
-def run(data_file, embedding_file, tmp_dir, interaction_type="phosphorylation"):
+def run(data_file, embedding_file, embed_dim, tmp_dir, interaction_type="phosphorylation"):
     logger = logging.getLogger(__name__)
     data = pd.read_json(data_file)
     class_size = 2
@@ -27,7 +27,7 @@ def run(data_file, embedding_file, tmp_dir, interaction_type="phosphorylation"):
 
     # TODO: For the moment just filter one type of classification and do binary as true vs false
     with open(embedding_file, "r") as embedding:
-        train_factory = RelationExtractionFactory(embedding, embedding_dim=300, class_size=class_size,
+        train_factory = RelationExtractionFactory(embedding, embedding_dim=embed_dim, class_size=class_size,
                                                   output_dir=tmp_dir, ngram=1)
         train_factory(X_train, y_train, X_test, y_test)
 
@@ -38,12 +38,14 @@ if "__main__" == __name__:
     parser.add_argument("inputjson",
                         help="The input json data")
     parser.add_argument("embedding", help="The embeeing file")
+    parser.add_argument("embeddim", help="the embed dim", type=int)
+
     parser.add_argument("outdir", help="The output dir")
     parser.add_argument("--interaction-type", help="The interction type", default="phosphorylation")
 
     args = parser.parse_args()
 
-    run(args.inputjson,args.embedding,
+    run(args.inputjson,args.embedding,args.embeddim,
         args.outdir, args.interaction_type)
 
 
