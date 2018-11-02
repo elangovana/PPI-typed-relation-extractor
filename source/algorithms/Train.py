244e69a4206eb23c9f73a43d09be546c7bb6e8c1
aeg <aeg@amazon.com>
2018-11-02 11:44:13 +1100

    Fix model accuracy

--- source/algorithms/Train.py
+++ source/algorithms/Train.py
@@ -14,7 +14,7 @@ class Train:
 
     def __call__(self, data_iter, validation_iter, text_sort_key_lambda, model_network, loss_function, optimizer,
                  output_dir,
-                 epoch=10, mini_batch_size=10,
+                 epoch=10, mini_batch_size=32,
                  eval_every_n_epoch=1, device_type="cpu"):
         """
 Runs train...
@@ -29,12 +29,12 @@ Runs train...
         :param optimizer: Optimiser
         """
         losses = []
-        n_correct, n_total, best_val_acc = 0, 0, -1
+        best_val_acc = -1
         start = datetime.datetime.now()
         iterations = 0
         val_log_template = ' '.join(
             '{:>6.0f},{:>5.0f},{:>9.0f},{:>5.0f}/{:<5.0f} {:>7.0f}%,{:>8.6f},{:8.6f},{:12.4f},{:12.4f}'.split(','))
-
+        val_log_template = "Run {}".format(val_log_template)
         train_iter, val_iter = BucketIterator.splits(
             (data_iter, validation_iter),  # we pass in the datasets we want the iterator to draw data from
             batch_size=mini_batch_size,
@@ -48,6 +48,7 @@ Runs train...
         for epoch in range(epoch):
             train_iter.init_epoch()
             total_loss = 0
+            n_correct, n_total = 0, 0
             for batch_x, batch_y in train_iter:
                 iterations += 1
                 # for feature, target in zip(batch_x, batch_y):
@@ -76,26 +77,23 @@ Runs train...
                 losses.append(total_loss)
 
                 n_correct += (torch.max(predicted, 1)[1].view(batch_y.size()) == batch_y).sum().item()
-                n_total += len(batch_x)
-
-                train_acc = 100. * n_correct / n_total
+                n_total += len(batch_y)
 
             # evaluate performance on validation set periodically
             if epoch % eval_every_n_epoch == 0:
+
+                train_acc = 100. * n_correct / n_total
 
                 val_acc, val_loss = self.calculate_val_loss(loss_function, model_network, val_iter, validation_iter)
 
                 print(val_log_template.format((datetime.datetime.now() - start).seconds,
                                               epoch, iterations, 1 + len(batch_x), len(train_iter),
-                                              100. * (1 + len(batch_x)) / len(train_iter), loss.item(),
+                                              100. * (1 + len(batch_x)) / len(train_iter), total_loss,
                                               val_loss.item(), train_acc, val_acc))
 
                 # update best valiation set accuracy
                 if val_acc > best_val_acc:
                     self.save_snapshot(model_network, output_dir)
-
-            # End of each epoch
-            print("{}\t{}".format(total_loss, train_acc))
 
     def calculate_val_loss(self, loss_function, model_network, val_iter, validation_iter):
         # switch model to evaluation mode
@@ -106,6 +104,7 @@ Runs train...
         with torch.no_grad():
             for val_batch_idx, val_y in val_iter:
                 answer = model_network(val_batch_idx)
+
                 n_val_correct += (torch.max(answer, 1)[1].view(val_y.size()) == val_y).sum().item()
                 val_loss = loss_function(answer, val_y)
         val_acc = 100. * n_val_correct / len(validation_iter)
