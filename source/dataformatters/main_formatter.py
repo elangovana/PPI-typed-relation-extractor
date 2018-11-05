90f1499d7b66e2ac5404b36ff5c4fbec3690d5f4
aeg <aeg@amazon.com>
2018-11-05 15:15:33 +1100

    Add formatter

---
+++ source/dataformatters/main_formatter.py
@@ -0,0 +1,32 @@
+import argparse
+import logging
+import sys
+
+import pandas as pd
+
+from dataformatters.pubtatorAbstractOnlyFormatter import PubtatorAbstractOnlyFormatter
+
+
+def run(input_json_file, output_json_file):
+    logger = logging.getLogger(__name__)
+
+    logger.info("Running inputs {}, {}".format(input_json_file, output_json_file))
+
+    data = pd.read_json(input_json_file)
+    abstract = lambda x: x.pubmedabstract
+    pubbmed = lambda x: x.pubmedId
+
+    with open(output_json_file, "w") as output_file:
+        PubtatorAbstractOnlyFormatter()(data.itertuples(), pubbmed, abstract, output_file)
+
+
+if "__main__" == __name__:
+    logging.basicConfig(level=logging.INFO, handlers=[logging.StreamHandler(sys.stdout)],
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputjson",
+                        help="The input json data file")
+    parser.add_argument("outdir", help="The output dir")
+    args = parser.parse_args()
+
+    run(args.inputjson, args.outdir)
