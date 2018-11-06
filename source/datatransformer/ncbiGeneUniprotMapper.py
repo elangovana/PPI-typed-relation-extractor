2e9c5ee4fb7d42ab19286f2d24f0c86417b79469
aeg <aeg@amazon.com>
2018-11-06 21:38:21 +1100

    Add more tests for uniprot mapper

--- source/datatransformer/ncbiGeneUniprotMapper.py
+++ source/datatransformer/ncbiGeneUniprotMapper.py
@@ -8,27 +8,34 @@ class NcbiGeneUniprotMapper:
 
     def __init__(self, url='https://www.uniprot.org/uploadlists/'):
         self.url = url
+        self._cache_dict = {}
 
     @property
     def logger(self):
         return logging.getLogger(__name__)
 
-    @lru_cache(maxsize=32)
     def convert(self, ids):
         if type(ids) is list:
-            ids = " ".join(ids)
+            return self._convert_list(ids)
+        else:
+            return self._convert(ids)
 
+    def _convert_list(self, ids):
+        ids = " ".join(ids)
+        results = self._convert(ids)
+        return results
+
+    @lru_cache(125)
+    def _convert(self, ids):
         params = {
             'from': 'P_ENTREZGENEID',
             'to': 'ACC',
             'format': 'tab',
             'query': ids
         }
-
         data = urlencode(params)
         response = urllib.request.urlopen("{}?{}".format(self.url, data))
         page = response.read().decode("utf-8")
-
         results = []
         for r in page.split("\n")[1:]:
             if r.strip("\s") == "": break
@@ -36,5 +43,4 @@ class NcbiGeneUniprotMapper:
             m = r.split("\t")
 
             results.append({m[0]: m[2]})
-
         return results
--- tests/test_datatransformer/IT_test_ncbiGeneUniprotMapper.py
+++ tests/test_datatransformer/IT_test_ncbiGeneUniprotMapper.py
@@ -9,12 +9,15 @@ from datatransformer.ncbiGeneUniprotMapper import NcbiGeneUniprotMapper
 class TestNcbiGeneUniprotMapper(TestCase):
 
     @data(('10076', 1)
+        , (['10076', '25930'], 3)
         , ('DUMMY', 0))
     @unpack
     def test_convert(self, geneid, expected_len):
         # Arrange
         sut = NcbiGeneUniprotMapper()
 
+        # Act
         actual = sut.convert(geneid)
 
+        # Assert
         self.assertEqual(len(actual), expected_len)
