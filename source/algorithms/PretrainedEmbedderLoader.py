f4fa93a366c36edf93c110a0670356d2cded8475
aeg <aeg@amazon.com>
2018-11-18 14:11:50 +1100

    Add logging

--- source/algorithms/PretrainedEmbedderLoader.py
+++ source/algorithms/PretrainedEmbedderLoader.py
@@ -1,9 +1,11 @@
-import numpy as np
-import torch
-from numpy.core.multiarray import ndarray
+import logging
 
 
 class PretrainedEmbedderLoader:
+
+    @property
+    def logger(self):
+        return logging.getLogger(__name__)
 
     def __call__(self, handle, other_words_embed_dict, ignore_word_filter=None):
         """
@@ -35,6 +37,8 @@ sandberger 0.072617 -0.51393 0.4728 -0.52202 -0.35534 0.34629 0.23211 0.23096 0.
             embeddings_array.append(embeddings)
 
         # Add  embeddings for additional words that do not exist in handle
+        words_not_in_embedding = set()
+
         for w in other_words_embed_dict.keys():
             # if word needs to be filtered, ignore word
             if ignore_word_filter is not None and ignore_word_filter(w): continue
@@ -43,6 +47,10 @@ sandberger 0.072617 -0.51393 0.4728 -0.52202 -0.35534 0.34629 0.23211 0.23096 0.
             if word_index_dict.get(w, None) is None:
                 word_index_dict[w] = len(word_index_dict)
                 embeddings_array.append(other_words_embed_dict[w])
+                words_not_in_embedding.add(w)
+
+        self.logger.info("The number of words intialised without embbeder is {}".format(len(words_not_in_embedding)))
+        self.logger.debug("The words intialised without embbeder is \n {}".format(words_not_in_embedding))
 
         # Convert to ndarray or cupy
         return word_index_dict, embeddings_array
