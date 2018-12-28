d8becca0bedee5c9fcd6739fcc132361a0f4f551
aeg <aeg@amazon.com>
2018-12-28 15:19:25 +1100

    Delete wordembeddings.py

--- tests/IT_embeddings.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from algorithms.WordEmbeddings import WordEmbeddings
-
-class TestEmbeddings(TestCase):
-    def test_run(self):
-        #Arrange
-        sut = WordEmbeddings("./GoogleNews-vectors-negative300.bin")
-
-        sut.run()
