39afbbe00be1f4c62c675957211107267d136c25
aeg <aeg@amazon.com>
2019-01-02 19:40:38 +1100

    Return max distance when entity not found

--- tests/test_algorithms/test_positionEmbedder.py
+++ tests/test_algorithms/test_positionEmbedder.py
@@ -74,6 +74,28 @@ Tests the returned position is the max distance if the distance is larger than  
         self.assertEqual(actual.shape, expected.shape)
         self.assertSequenceEqual(actual.ravel().tolist(), expected.ravel().tolist())
 
+    def test__call__case_no_matching_entity(self):
+        """
+Tests case where the entity is not present in the text
+        """
+        # Arrange
+        max_pos = 2
+        pos_dim = 2
+        position_embeddings = np.random.uniform(0, 1, (max_pos, pos_dim))
+        # Input data
+        tokens_array = range(0, max_pos)
+        entity = max_pos + 10
+        # Expected output
+        expected = np.asarray(
+            [position_embeddings[max_pos - 1]] * pos_dim)
+
+        # Act
+        actual = self._invoke_sut__call__(entity, position_embeddings, tokens_array)
+
+        # Assert
+        self.assertEqual(actual.shape, expected.shape)
+        self.assertSequenceEqual(actual.ravel().tolist(), expected.ravel().tolist())
+
     def _invoke_sut__call__(self, entity, position_embeddings, tokens_array):
         # Initialise sut
         sut = PositionEmbedder(embeddings=position_embeddings)
