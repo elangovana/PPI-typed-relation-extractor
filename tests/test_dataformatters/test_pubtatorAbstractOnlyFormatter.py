ff1632871f8dfc5b1318d5985bee05867defeb73
aeg <aeg@amazon.com>
2019-04-09 07:34:10 -0700

    Include unicode clean test

--- tests/test_dataformatters/test_pubtatorAbstractOnlyFormatter.py
+++ tests/test_dataformatters/test_pubtatorAbstractOnlyFormatter.py
@@ -22,3 +22,21 @@ class TestPubtatorAbstractOnlyFormatter(TestCase):
 
         # Assert
         self.assertEqual(output.getvalue(), expected)
+
+    def test___call__cleanuincode(self):
+        """
+    Verifies the formatting
+            """
+        # Arrange
+        sut = PubtatorAbstractOnlyFormatter()
+        data_iter = [{"pubmedid": "1233", "abstract": "This is a \u2808cat"},
+                     {"pubmedid": "12133", "abstract": "This is a dog"}]
+        output = StringIO()
+
+        expected = "1233|a|This is a cat\n\n12133|a|This is a dog\n\n"
+
+        # Act
+        sut(data_iter, lambda x: x["pubmedid"], lambda x: x["abstract"], output)
+
+        # Assert
+        self.assertEqual(output.getvalue(), expected)
