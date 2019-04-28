ea8ff337ff63e2a2f70e5e748ff204620d101861
aeg <aeg@amazon.com>
2019-04-28 11:14:22 +1000

    Refactor predict

--- source/algorithms/main_predict.py
+++ source/algorithms/main_predict.py
@@ -3,7 +3,6 @@ import logging
 import os
 import sys
 
-import numpy as np
 import pandas as pd
 
 from algorithms.RelationExtractionAverageFactory import RelationExtractionAverageFactory
@@ -27,51 +26,54 @@ model_dict = {
 
 
 def prepare_data(data_df):
-    labels = data_df[["isValid"]]
     data_df = data_df[["normalised_abstract", "interactionType", "participant1Id", "participant2Id"]]
     # data_df['participant1Alias'] = data_df['participant1Alias'].map(
     #     lambda x: ", ".join(list(itertools.chain.from_iterable(x))))
     # data_df['participant2Alias'] = data_df['participant2Alias'].map(
     #     lambda x: ", ".join(list(itertools.chain.from_iterable(x))))
-    labels = np.reshape(labels.values.tolist(), (-1,))
-    return data_df.copy(deep=True), labels
+
+    return data_df.copy(deep=True)
 
 
 def run(network, data_file, artifactsdir, out_dir):
+    logger = logging.getLogger(__name__)
+
+    final_df = run_prediction(artifactsdir, data_file, network, out_dir)
+    predictions_file = os.path.join(out_dir, "predicted.json")
+
+    final_df.to_json(predictions_file)
+
+    logger.info("Completed {}, {}".format(final_df.shape, final_df.columns.values))
+    return final_df
+
+
+def run_prediction(artifactsdir, data_file, network, out_dir):
     logger = logging.getLogger(__name__)
 
     if not os.path.exists(out_dir) or not os.path.isdir(out_dir):
         raise FileNotFoundError("The path {} should exist and must be a directory".format(out_dir))
 
     logger.info("Running with self relations filter network {}".format(network))
-
     logger.info("Loading from file {}".format(data_file))
     df = pd.read_json(data_file)
-    logger.info("Data size after load: {}".format(df.shape))
 
-    df_prep, labels = prepare_data(df)
+    logger.info("Data size after load: {}".format(df.shape))
+    df_prep = prepare_data(df)
     logger.info("Data size after prep: {}".format(df_prep.shape))
 
     network_factory = networks_dict[network]
     if network in model_dict:
         network_factory.model_network = model_dict[network]
-
     predictor = network_factory.load(artifactsdir)
-    results, cofidence_scores = predictor(df_prep)
 
+    # Run prediction
+    results, confidence_scores = predictor(df_prep)
     df_prep["predicted"] = results
-    df_prep["confidence_scores"] = cofidence_scores
-
-    predictions_file = os.path.join(out_dir, "predicted.json")
+    df_prep["confidence_scores"] = confidence_scores
     select_columns = df.columns.values
-
     final_df = df[select_columns].merge(df_prep[["predicted", "confidence_scores"]], how='inner', left_index=True,
                                         right_index=True)
-
-    final_df.to_json(predictions_file)
-
-    logger.info("Completed {}, {}".format(final_df.shape, final_df.columns.values))
-    return results
+    return final_df
 
 
 if "__main__" == __name__:
