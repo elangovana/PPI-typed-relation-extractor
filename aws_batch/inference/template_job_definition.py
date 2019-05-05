e5837b03baf15ff137ff3f7f2ba927147339c313
aeg <aeg@amazon.com>
2019-05-05 10:22:24 +1000

    Add inference batch

---
+++ aws_batch/inference/template_job_definition.py
@@ -0,0 +1,70 @@
+def get_job_definition(account, region, container_name, job_def_name, job_param_s3uri_destination, memoryInMB, ncpus,
+                       role_name):
+    """
+This is the job definition for this sample job.
+    :param account:
+    :param region:
+    :param container_name:
+    :param job_def_name:
+    :param memoryInMB:
+    :param ncpus:
+    :param role_name:
+    :return:
+    """
+    return {
+        "jobDefinitionName": job_def_name,
+        "type": "container",
+        # These are the arguments for the job
+        "parameters": {
+            "localpath": "/data",
+            "s3destination": job_param_s3uri_destination,
+            "s3src": job_param_s3uri_destination,
+            "s3network": job_param_s3uri_destination,
+            "networktype": "CnnPos"
+        },
+        # Specify container & jobs properties include entry point and job args that are referred to in parameters
+        "containerProperties": {
+            "image": container_name,
+            "vcpus": ncpus,
+            "memory": memoryInMB,
+            "command": [
+                "bash",
+                "scripts/inference.sh",
+                "Ref::s3src",
+                "Ref::s3destination",
+                "Ref::s3network",
+                "Ref::networktype",
+                "Ref::localpath"
+
+            ],
+            "jobRoleArn": "arn:aws:iam::{}:role/{}".format(account, role_name),
+            "volumes": [
+                {
+                    "host": {
+                        "sourcePath": job_def_name
+                    },
+                    "name": "data"
+                }
+            ],
+            "environment": [
+                {
+                    "name": "AWS_DEFAULT_REGION",
+                    "value": region
+                }
+            ],
+            "mountPoints": [
+                {
+                    "containerPath": "/data",
+                    "readOnly": False,
+                    "sourceVolume": "data"
+                }
+            ],
+            "readonlyRootFilesystem": False,
+            "privileged": True,
+            "ulimits": [],
+            "user": ""
+        },
+        "retryStrategy": {
+            "attempts": 5
+        }
+    }
---
+++ source/scripts/inference.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+src_s3=$1
+dest_s3=$2
+network_s3=$3
+network_type=$4
+local_path=$5
+
+set -e
+set -x
+
+tmp_dir=$(python -c 'import sys,uuid; sys.stdout.write(uuid.uuid4().hex)')
+
+src_local_path=${local_path}/${tmp_dir}/input
+src_file_name=$(echo ${src_s3} | rev | cut -d/ -f1 | rev)
+src_local_file=${src_local_path}/${src_file_name}
+
+network_local_path=${local_path}/${tmp_dir}/dat
+dest_local_path=${local_path}/${tmp_dir}
+
+
+mkdir -p ${src_local_path}
+mkdir -p ${network_local_path}
+mkdir -p ${dest_local_path}
+
+# install aws s3
+pip3 install awscli
+
+# Copy data from s3
+aws s3 cp ${src_s3} ${src_local_path}
+aws s3 cp ${network_s3} ${network_local_path} --recursive --exclude "*"  --include "*.json" --exclude "*vocab.json" --include "*.log"
+
+# Run
+scripts_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+source_dir=${scripts_dir}/..
+export PYTHONPATH=${source_dir}
+
+head -1 ${network_local_path}/run.log
+head -10 ${network_local_path}/run.log | tail -8
+python ${source_dir}/algorithms/main_predict.py ${network_type}  ${src_local_file} ${dest_local_path}  ${network_local_path}  ${dest_local_path}
+mv ${dest_local_path}/predicted.json ${dest_local_path}/${src_file_name}.prediction.json
+
+# Copy results back s3
+aws s3 cp --recursive ${dest_local_path}/ ${dest_s3}
+
+# Clean up
+rm -rf ${dest_local_path}
+rm -rf ${src_local_path}
+rm -rf ${network_local_path}
