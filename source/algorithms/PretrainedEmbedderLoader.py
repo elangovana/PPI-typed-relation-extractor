c56d3a3ace0077357d31e7129d2d2d633b35e7ac
aeg <aeg@amazon.com>
2019-08-13 14:37:07 +1000

    Fix pretrained

--- source/algorithms/PretrainedEmbedderLoader.py
+++ source/algorithms/PretrainedEmbedderLoader.py
@@ -1,5 +1,6 @@
 import logging
 
+import numpy as np
 from torch import nn
 
 
@@ -9,22 +10,39 @@ class PretrainedEmbedderLoader:
     def logger(self):
         return logging.getLogger(__name__)
 
-    def __call__(self, handle, other_words_embed=None, ignore_word_filter=None):
+    def get_vocab_dict(self, handle):
+        vocab_dict = {}
+        for i, line in enumerate(handle):
+            # skip first line as it contains just the dim
+            if i == 0: continue
+            values = line.split()
+            word = values[0]
+            vocab_dict[word] = vocab_dict.get(word, len(vocab_dict))
+        return vocab_dict
+
+    def __call__(self, handle, initial_words_index_dict=None):
         """
 Expects the stream of strings to contain word embedding. Each record must be in space separated format with the first column containing the word itself.
 Each record is separated by new lines
 e.g for an embedding of size 10
 zsombor -0.75898 -0.47426 0.4737 0.7725 -0.78064 0.23233 0.046114 0.84014 0.243710 .022978
 sandberger 0.072617 -0.51393 0.4728 -0.52202 -0.35534 0.34629 0.23211 0.23096 0.26694 .41028
-        :param other_words_embed: Additional words word embedding, for words not in the handle
-        :param ignore_word_filter: Use a filter function to ignore words, return true to ignore a word
+        :param words_index_dict: The index of words so the correct embedding is assigned
         :return: a tuple (word_index_dict, embeddings_array)
         :param handle: handle containing the embedding
         """
-        word_index_dict = {}
-        embeddings_array = []
-        other_words_embed = other_words_embed or {}
+        initial_words_index_dict = initial_words_index_dict or {}
 
+        if len(initial_words_index_dict) > 0:
+            assert len(initial_words_index_dict) - 1 == max(
+                initial_words_index_dict.values()), "The word index dict must be values between 0 to len(dict) {}, but found max value to be {}".format(
+                len(initial_words_index_dict) - 1,
+                max(initial_words_index_dict.values()))
+            assert min(initial_words_index_dict.values()) == 0, "The word index dict must be zero indexed values"
+
+        embeddings_array = [[]] * len(initial_words_index_dict)
+
+        result_words_index_dict = {}
         # Load embeddings from file
         for i, line in enumerate(handle):
             # skip first line as it contains just the dim
@@ -32,30 +50,32 @@ sandberger 0.072617 -0.51393 0.4728 -0.52202 -0.35534 0.34629 0.23211 0.23096 0.
             values = line.split()
             word = values[0]
 
-            # if word needs to be filtered, ignore word
-            if ignore_word_filter is not None and ignore_word_filter(word): continue
-
             # Not ignored word
             embeddings = [float(v) for v in values[1:]]
-            word_index_dict[word] = len(word_index_dict)
-            embeddings_array.append(embeddings)
+            if word not in initial_words_index_dict:
+                result_words_index_dict[word] = len(embeddings_array)
+                embeddings_array.append([])
+            else:
+                result_words_index_dict[word] = initial_words_index_dict[word]
 
-        # Add  embeddings for additional words that do not exist in handle
-        words_not_in_embedding = set()
+            embeddings_array[result_words_index_dict[word]] = embeddings
+
         embedding_dim = len(embeddings_array[0])
-        for w in other_words_embed:
-            # if word needs to be filtered, ignore word
-            if ignore_word_filter is not None and ignore_word_filter(w): continue
-
-            # Not ignored word
-            if word_index_dict.get(w, None) is None:
-                word_index_dict[w] = len(word_index_dict)
-                random_embedding = nn.Embedding(1, embedding_dim).weight.detach().numpy().tolist()[0]
-                embeddings_array.append(random_embedding)
-                words_not_in_embedding.add(w)
+        words_not_in_embedding = []
+        for k in initial_words_index_dict:
+            # Word not found in embedding, init with random
+            if embeddings_array[initial_words_index_dict[k]] == []:
+                result_words_index_dict[k] = initial_words_index_dict[k]
+                words_not_in_embedding.append(k)
+                embeddings_array[initial_words_index_dict[k]] = \
+                    nn.Embedding(1, embedding_dim).weight.detach().numpy().tolist()[
+                        0]
 
         self.logger.info("The number of words intialised without embbeder is {}".format(len(words_not_in_embedding)))
         self.logger.debug("The words intialised without embbeder is \n {}".format(words_not_in_embedding))
 
         # Convert to ndarray or cupy
-        return word_index_dict, embeddings_array
+        embeddings_array = np.array(embeddings_array)
+        print(embeddings_array)
+        print(result_words_index_dict)
+        return result_words_index_dict, embeddings_array
---
+++ tests/test_algorithms/test_pretrainedEmbedderLoader.py
@@ -0,0 +1,84 @@
+from io import StringIO
+from unittest import TestCase
+
+from algorithms.PretrainedEmbedderLoader import PretrainedEmbedderLoader
+
+
+class TestPretrainedEmbedderLoader(TestCase):
+    def test___call__simplecase(self):
+        embed_dim = 3
+        embeddings_list = ["4 3",
+                           "hat 0.2 .34 0.8",
+                           "mat 0.5 .34 0.8",
+                           "entity1 0.5 .55 0.8",
+                           "entity2 0.3 .55 0.9"]
+        embedding_handle = StringIO(
+            "\n".join(embeddings_list))
+
+        sut = PretrainedEmbedderLoader()
+
+        # Act
+        word_index_dict, embeddings_array = sut(embedding_handle, initial_words_index_dict=None)
+
+        # Assert
+        self.assertEqual(embeddings_array.shape[0], len(word_index_dict))
+        self.assertEqual(len(embeddings_list) - 1, len(word_index_dict))
+        self.assertEqual((len(embeddings_list) - 1, embed_dim), embeddings_array.shape)
+        self.assertSequenceEqual(embeddings_array[word_index_dict["hat"]].tolist(), [0.2, .34, 0.8])
+
+    def test___call__worddict(self):
+        # Arrange
+        # Construct embedding
+        embed_dim = 3
+        embeddings_list = ["4 3",
+                           "hat 0.2 .34 0.8",
+                           "mat 0.5 .34 0.8",
+                           "entity1 0.5 .55 0.8",
+                           "entity2 0.3 .55 0.9"]
+        embedding_handle = StringIO(
+            "\n".join(embeddings_list))
+        existing_word_dict = {"entity1": 0}
+
+        embed_words_size = len(embeddings_list) - 1
+
+        sut = PretrainedEmbedderLoader()
+
+        # Act
+        word_index_dict, embeddings_array = sut(embedding_handle, initial_words_index_dict=existing_word_dict)
+
+        # Assert
+        print(word_index_dict, embeddings_array)
+        self.assertEqual(embeddings_array.shape[0], len(word_index_dict))
+        self.assertEqual(embed_words_size, len(word_index_dict))
+        self.assertEqual((embed_words_size, embed_dim), embeddings_array.shape)
+        self.assertSequenceEqual(embeddings_array[existing_word_dict["entity1"]].tolist(), [0.5, .55, 0.8])
+
+    def test___call__worddict_with_new_words(self):
+        """
+        Test with new words not in embedding
+        :return:
+        """
+        embed_dim = 3
+        embeddings_list = ["4 3",
+                           "hat 0.2 .34 0.8",
+                           "mat 0.5 .34 0.8",
+                           "entity1 0.5 .55 0.8",
+                           "entity2 0.3 .55 0.9"]
+        embedding_handle = StringIO(
+            "\n".join(embeddings_list))
+        # Additional dictionary passed in with words not found in the embeddings handle
+        existing_word_dict = {"entity1": 0, "newword": 1}
+
+        embed_words_size = len(embeddings_list) - 1
+        # One new word in the existing word dictionary
+        total_words = embed_words_size + 1
+        sut = PretrainedEmbedderLoader()
+
+        # Act
+        word_index_dict, embeddings_array = sut(embedding_handle, initial_words_index_dict=existing_word_dict)
+
+        # Assert
+        print(embeddings_array)
+        self.assertEqual(total_words, len(word_index_dict))
+        self.assertEqual((total_words, embed_dim), embeddings_array.shape)
+        self.assertEqual(embed_dim, len(embeddings_array[existing_word_dict["newword"]].tolist()))
