36f3e76194708ae6395c066a342e14dbd73e1a98
aeg <aeg@amazon.com>
2019-08-20 20:05:50 +1000

    Refactor inference

---
+++ source/algorithms/InferencePipeline.py
@@ -0,0 +1,66 @@
+import logging
+import math
+import os
+
+import pandas as pd
+
+from algorithms.PpiDataset import PPIDataset
+from algorithms.TrainInferencePipeline import TrainInferencePipeline
+
+
+class InferencePipeline:
+    def run(self, data_file, artifactsdir, out_dir, postives_filter_threshold=0.0):
+        logger = logging.getLogger(__name__)
+
+        final_df = self.run_prediction(artifactsdir, data_file, out_dir)
+
+        logger.info("Completed {}, {}".format(final_df.shape, final_df.columns.values))
+
+        final_df = self._filter_threshold(final_df, postives_filter_threshold)
+
+        predictions_file = os.path.join(out_dir, "predicted.json")
+        final_df.to_json(predictions_file)
+
+        return final_df
+
+    def _filter_threshold(self, final_df, postives_filter_threshold):
+        logger = logging.getLogger(__name__)
+
+        if postives_filter_threshold == 0.0:
+            return final_df
+
+        logger.info(
+            "Filtering True Positives with threshold > {}, currently {} records".format(postives_filter_threshold,
+                                                                                        final_df.shape))
+        final_df = final_df.query("confidence_true >= {}".format(postives_filter_threshold))
+        logger.info("Post filter shape {}".format(final_df.shape))
+        return final_df
+
+    def run_prediction(self, artifactsdir, data_file, out_dir):
+        logger = logging.getLogger(__name__)
+
+        if not os.path.exists(out_dir) or not os.path.isdir(out_dir):
+            raise FileNotFoundError("The path {} should exist and must be a directory".format(out_dir))
+
+        logger.info("Loading from file {}".format(data_file))
+
+        df = pd.read_json(data_file)
+
+        predictor = TrainInferencePipeline.load(artifactsdir)
+        val_dataset = self.get_dataset(data_file)
+
+        # Run prediction
+        results, confidence_scores = predictor(val_dataset)
+        df["predicted"] = results
+        df["confidence_scores"] = confidence_scores
+
+        # This is log softmax, convert to softmax prob
+
+        df["confidence_true"] = df.apply(lambda x: math.exp(x["confidence_scores"][True]), axis=1)
+        df["confidence_false"] = df.apply(lambda x: math.exp(x["confidence_scores"][False]), axis=1)
+
+        return df
+
+    def get_dataset(self, data_file):
+        val_dataset = PPIDataset(data_file)
+        return val_dataset
--- source/algorithms/main_predict.py
+++ source/algorithms/main_predict.py
@@ -1,61 +1,8 @@
 import argparse
 import logging
-import math
-import os
 import sys
 
-import pandas as pd
-
-from algorithms.PpiDataset import PPIDataset
-from algorithms.TrainInferencePipeline import TrainInferencePipeline
-
-
-def run(data_file, artifactsdir, out_dir, postives_filter_threshold=0.0):
-    logger = logging.getLogger(__name__)
-
-    final_df = run_prediction(artifactsdir, data_file, out_dir)
-
-    logger.info("Completed {}, {}".format(final_df.shape, final_df.columns.values))
-
-    if postives_filter_threshold > 0.0:
-        logger.info(
-            "Filtering True Positives with threshold > {}, currently {} records".format(postives_filter_threshold,
-                                                                                        final_df.shape))
-        final_df = final_df.query("confidence_true >= {}".format(postives_filter_threshold))
-        logger.info("Post filter shape {}".format(final_df.shape))
-
-    predictions_file = os.path.join(out_dir, "predicted.json")
-    final_df.to_json(predictions_file)
-
-    return final_df
-
-
-def run_prediction(artifactsdir, data_file, out_dir):
-    logger = logging.getLogger(__name__)
-
-    if not os.path.exists(out_dir) or not os.path.isdir(out_dir):
-        raise FileNotFoundError("The path {} should exist and must be a directory".format(out_dir))
-
-    logger.info("Loading from file {}".format(data_file))
-
-    df = pd.read_json(data_file)
-
-    predictor = TrainInferencePipeline.load(artifactsdir)
-    val_dataset = PPIDataset(data_file)
-
-    # Run prediction
-    results, confidence_scores = predictor(val_dataset)
-    print(confidence_scores)
-    df["predicted"] = results
-    df["confidence_scores"] = confidence_scores
-
-    # This is log softmax, convert to softmax prob
-
-    df["confidence_true"] = df.apply(lambda x: math.exp(x["confidence_scores"][True]), axis=1)
-    df["confidence_false"] = df.apply(lambda x: math.exp(x["confidence_scores"][False]), axis=1)
-
-    return df
-
+from algorithms.InferencePipeline import InferencePipeline
 
 if "__main__" == __name__:
     parser = argparse.ArgumentParser()
@@ -77,5 +24,5 @@ if "__main__" == __name__:
     logging.basicConfig(level=logging.getLevelName(args.log_level), handlers=[logging.StreamHandler(sys.stdout)],
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    results = run(args.datajson, args.artefactsdir,
-                  args.outdir, args.positives_filter_threshold)
+    results = InferencePipeline().run(args.datajson, args.artefactsdir,
+                                      args.outdir, args.positives_filter_threshold)
