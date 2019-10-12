1e001c69b9f628392407d5f3b24f0dd1d7571584
aeg <aeg@amazon.com>
2019-10-12 17:51:55 +1100

    Update comments

--- source/algorithms/BiobertTfConverter.py
+++ source/algorithms/BiobertTfConverter.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from shutil import copyfile
 
 from pytorch_pretrained_bert.convert_tf_checkpoint_to_pytorch import convert_tf_checkpoint_to_pytorch
 
@@ -8,11 +9,17 @@ class BioBertConverter:
     """
     Wrapper around pytorch_pretrained_bert.convert_tf_checkpoint_to_pytorch  to convert Pytorch pretrained bert to pytorch
     """
+
     def __call__(self, base_dir, output_dir):
+        config_file = os.path.join(base_dir, 'bert_config.json')
+        config_file_dest = os.path.join(output_dir, 'bert_config.json')
+        # Convert
         convert_tf_checkpoint_to_pytorch(
             os.path.join(base_dir, 'model.ckpt-1000000'),
-            os.path.join(base_dir, 'bert_config.json'),
-            os.path.join(output_dir, 'pytorch_bio_bert_model.bin'))
+            config_file,
+            os.path.join(output_dir, 'pytorch_model.bin'))
+        # copy config file
+        copyfile(config_file, config_file_dest)
 
 
 if "__main__" == __name__:
