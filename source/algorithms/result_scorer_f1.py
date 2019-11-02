ee659930558a01875ad87e8d37913e8dbb1f6246
aeg <aeg@amazon.com>
2019-11-02 17:26:28 +1100

    Use micro average

--- source/algorithms/result_scorer_f1.py
+++ source/algorithms/result_scorer_f1.py
@@ -12,6 +12,6 @@ class ResultScorerF1:
     def __call__(self, y_actual, y_pred, pos_label):
         from sklearn.metrics import f1_score
 
-        f1 = f1_score(y_actual, y_pred, pos_label=pos_label, average='micro')
+        f1 = f1_score(y_actual, y_pred, pos_label=pos_label, average='macro')
 
         return f1
