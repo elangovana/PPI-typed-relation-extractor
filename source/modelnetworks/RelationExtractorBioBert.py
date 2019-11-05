1ceddcecd1cae46ea0acf042d53699c74ba7272a
aeg <aeg@amazon.com>
2019-11-05 13:43:12 +1100

    Add logging

--- source/modelnetworks/RelationExtractorBioBert.py
+++ source/modelnetworks/RelationExtractorBioBert.py
@@ -1,11 +1,23 @@
+import logging
+
+import torch
 from pytorch_pretrained_bert.modeling import BertForSequenceClassification
 from torch import nn
 
 
 class RelationExtractorBioBert(nn.Module):
 
-    def __init__(self, model_dir, num_classes):
+    @property
+    def logger(self):
+        return logging.getLogger(__name__)
+
+    def __init__(self, model_dir, num_classes, seed=None):
         super().__init__()
+
+        if seed is None:
+            seed = torch.initial_seed() & ((1 << 63) - 1)
+        self.logger.info("Using seed {}".format(seed))
+        torch.manual_seed(seed)
 
         self.model = BertForSequenceClassification.from_pretrained(model_dir,
                                                                    num_labels=num_classes)
