d6e17e6341abaed74e40b72c947b19f926100e29
aeg <aeg@amazon.com>
2020-01-19 20:39:31 +1100

    support ensemble

--- source/algorithms/InferencePipeline.py
+++ source/algorithms/InferencePipeline.py
@@ -1,3 +1,4 @@
+import glob
 import logging
 import math
 import os
@@ -11,18 +12,26 @@ class InferencePipeline:
     def run(self, dataset, data_file, artifactsdir, out_dir, postives_filter_threshold=0.0):
         logger = logging.getLogger(__name__)
 
-        final_df = self.run_prediction(dataset, artifactsdir, data_file, out_dir)
+        predicted_confidence_field = "predicted_confidence"
+        predicted_output_field = "predicted"
+
+        final_df = self.run_prediction(dataset, artifactsdir, data_file, out_dir,
+                                       predicted_confidence_field=predicted_confidence_field,
+                                       predicted_field=predicted_output_field)
 
         logger.info("Completed {}, {}".format(final_df.shape, final_df.columns.values))
 
-        final_df = self._filter_threshold(final_df, postives_filter_threshold)
+        final_df = self._filter_threshold(final_df, postives_filter_threshold, dataset.lambda_postive_field_filter,
+                                          confidence_score_field=predicted_confidence_field,
+                                          predicted_field=predicted_output_field)
 
         predictions_file = os.path.join(out_dir, "predicted.json")
         final_df.to_json(predictions_file)
 
         return final_df
 
-    def _filter_threshold(self, final_df, postives_filter_threshold):
+    def _filter_threshold(self, final_df, postives_filter_threshold, filter_lambda,
+                          confidence_score_field="predicted_confidence", predicted_field="predicted_field"):
         logger = logging.getLogger(__name__)
 
         if postives_filter_threshold == 0.0:
@@ -31,11 +40,18 @@ class InferencePipeline:
         logger.info(
             "Filtering True Positives with threshold > {}, currently {} records".format(postives_filter_threshold,
                                                                                         final_df.shape))
-        final_df = final_df.query("confidence_true >= {}".format(postives_filter_threshold))
+
+        final_df = final_df[final_df.apply(
+            lambda x: filter_lambda(x[predicted_field]) and x[confidence_score_field] > postives_filter_threshold,
+            axis=1)]
+
         logger.info("Post filter shape {}".format(final_df.shape))
         return final_df
 
-    def run_prediction(self, dataset, artifactsdir, data_file, out_dir):
+    def run_prediction(self, dataset, artifactsdir, data_file, out_dir,
+                       confidence_scores_dict_field="confidence_scores",
+                       predicted_confidence_field="predicted_confidence",
+                       predicted_field="predicted"):
         logger = logging.getLogger(__name__)
 
         if not os.path.exists(out_dir) or not os.path.isdir(out_dir):
@@ -45,17 +61,22 @@ class InferencePipeline:
 
         df = pd.read_json(data_file)
 
-        predictor = TrainInferencePipeline.load(artifactsdir)
+        ensemble_artefacts_dir = [d for d in glob.glob("{}{}*".format(artifactsdir, os.path.sep)) if os.path.isdir(d)]
+
+        predictor = TrainInferencePipeline.load_ensemble(ensemble_artefacts_dir)
 
         # Run prediction
         results, confidence_scores = predictor(dataset)
-        df["predicted"] = results
-        df["confidence_scores"] = confidence_scores
+        df[predicted_field] = results
+        df[confidence_scores_dict_field] = confidence_scores
+
+        keys = list(df[confidence_scores_dict_field].iloc[0].keys())
 
         # This is log softmax, convert to softmax prob
+        for k in keys:
+            df[k] = df.apply(lambda x: math.exp(x[confidence_scores_dict_field][k]), axis=1)
 
-        df["confidence_true"] = df.apply(lambda x: math.exp(x["confidence_scores"][True]), axis=1)
-        df["confidence_false"] = df.apply(lambda x: math.exp(x["confidence_scores"][False]), axis=1)
+        df[predicted_field] = df.apply(lambda r: keys[list(r[keys]).index(max(r[keys]))], axis=1)
+        df[predicted_confidence_field] = df.apply(lambda r: max(r[keys] / sum(r[keys])), axis=1)
 
         return df
-
--- source/algorithms/main_predict.py
+++ source/algorithms/main_predict.py
@@ -5,6 +5,16 @@ import sys
 from algorithms.InferencePipeline import InferencePipeline
 from algorithms.dataset_factory import DatasetFactory
 
+
+def run(dataset, datajson, artefactsbase_dir, outdir, positives_filter_threshold):
+    dataset_factory = DatasetFactory().get_datasetfactory(dataset)
+    dataset = dataset_factory.get_dataset(datajson)
+
+    # artifacts_list = [os.path.join(artefactsbase_dir, d) for d in os.listdir(artefactsbase_dir) if os.path.isdir(d)]
+
+    InferencePipeline().run(dataset, datajson, artefactsbase_dir, outdir, positives_filter_threshold)
+
+
 if "__main__" == __name__:
     parser = argparse.ArgumentParser()
 
@@ -13,7 +23,7 @@ if "__main__" == __name__:
     parser.add_argument("datajson",
                         help="The json data to predict")
 
-    parser.add_argument("artefactsdir", help="The artefacts dir that contains model, vocab etc")
+    parser.add_argument("artefactsdir", help="The base of artefacts dir that contains directories of model, vocab etc")
     parser.add_argument("outdir", help="The output dir")
 
     parser.add_argument("--log-level", help="Log level", default="INFO", choices={"INFO", "WARN", "DEBUG", "ERROR"})
@@ -23,11 +33,9 @@ if "__main__" == __name__:
     args = parser.parse_args()
 
     print(args.__dict__)
+
     # Set up logging
     logging.basicConfig(level=logging.getLevelName(args.log_level), handlers=[logging.StreamHandler(sys.stdout)],
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-    dataset_factory = DatasetFactory().get_datasetfactory(args.dataset)
-    dataset = dataset_factory.get_dataset(args.datajson)
-    results = InferencePipeline().run(dataset, args.datajson, args.artefactsdir,
-                                      args.outdir, args.positives_filter_threshold)
+    run(args.dataset, args.datajson, args.artefactsdir, args.outdir, args.positives_filter_threshold)
