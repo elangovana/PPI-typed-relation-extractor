400b34bd931d3e0ece531f89320eb9d33a217521
aeg <aeg@amazon.com>
2020-01-26 14:23:41 +1100

    Case where imex not found

--- source/datatransformer/ImexDataTransformerAugmentAbstract.py
+++ source/datatransformer/ImexDataTransformerAugmentAbstract.py
@@ -1,9 +1,12 @@
+import logging
+from multiprocessing.dummy import Pool
+
 from dataextractors.PubmedAbstractExtractor import PubmedAbstractExtractor
 
 
 class ImexDataTransformerAugmentAbstract:
     def __init__(self, id_key: str = "pubmedId",
-                 abstract_key: str = "pubmedabstract"):
+                 abstract_key: str = "pubmedabstract", pool_size=None):
         """
         :param id_key: The key in the input identifying the pubmedid used to extract the abstract
         :param abstract_key: The key to use for the resulting abstract
@@ -11,6 +14,8 @@ class ImexDataTransformerAugmentAbstract:
         self.abstract_key = abstract_key
         self.id_key = id_key
         self.pubmed_extractor = None
+        self.pool_size = 2
+        self.logger.info("Using pool size of {}".format(self.pool_size))
 
     @property
     def pubmed_extractor(self):
@@ -21,14 +26,48 @@ class ImexDataTransformerAugmentAbstract:
     def pubmed_extractor(self, value):
         self.__pubmed_extractor__ = value
 
+    @property
+    def logger(self):
+        return logging.getLogger(__name__)
+
+    #     def transform(self, input_iterable: iter) -> iter:
+    #         """
+    # Adds the pubmed abstract to each item based on the pubmed id
+    #         :param input_iterable: A iterable list of dictionary
+    #          """
+    #         for item in input_iterable:
+    #             pubmed_details_list = self.pubmed_extractor.extract_abstract_by_pubmedid([item[self.id_key]])
+    #
+    #             if len(pubmed_details_list) == 0:
+    #                 self.logger.warning("Could not find abstract for record {}".format(item[self.id_key]))
+    #                 yield None
+    #
+    #             # expect only one record in the array
+    #             assert len(pubmed_details_list) == 1
+    #             item[self.abstract_key] = pubmed_details_list[0]["abstract"]
+    #             yield item
+
     def transform(self, input_iterable: iter) -> iter:
         """
 Adds the pubmed abstract to each item based on the pubmed id
         :param input_iterable: A iterable list of dictionary
          """
-        for item in input_iterable:
-            pubmed_details_list = self.pubmed_extractor.extract_abstract_by_pubmedid([item[self.id_key]])
-            # expect only one record in the array
-            assert len(pubmed_details_list) == 1
-            item[self.abstract_key] = pubmed_details_list[0]["abstract"]
-            yield item
+
+        p = Pool(self.pool_size)
+        result = p.map(self._get_pubmed_abstract, input_iterable)
+
+        # remove nones from the list when the abstract could not be found
+        result = filter(None, result)
+
+        return result
+
+    def _get_pubmed_abstract(self, item):
+        pubmed_details_list = self.pubmed_extractor.extract_abstract_by_pubmedid([item[self.id_key]])
+        if len(pubmed_details_list) == 0:
+            self.logger.warning("Could not find abstract for record {}".format(item[self.id_key]))
+            return
+
+        # expect only one record in the array
+        assert len(pubmed_details_list) == 1, "Expecting just 1,  but found {}".format(len(pubmed_details_list))
+        item[self.abstract_key] = pubmed_details_list[0]["abstract"]
+        return item
--- tests/test_datatransformer/test_imexDataTransformerAugmentAbstract.py
+++ tests/test_datatransformer/test_imexDataTransformerAugmentAbstract.py
@@ -28,3 +28,21 @@ class TestImexDataTransformerAugmentAbstract(TestCase):
 
         # Assert
         self.assertSequenceEqual(actual, expected)
+
+    def test_should_transform_none(self):
+        # Arrange
+        key_pubmedid = "pubmedid"
+        key_pubmedabstract = "pubmedabstract"
+        sut = ImexDataTransformerAugmentAbstract(id_key=key_pubmedid, abstract_key=key_pubmedabstract)
+        input = [{key_pubmedid: "1234"}]
+        expected = []
+
+        # mock
+        sut.pubmed_extractor = MagicMock()
+        sut.pubmed_extractor.extract_abstract_by_pubmedid.return_value = []
+
+        # Act
+        actual = list(sut.transform(input))
+
+        # Assert
+        self.assertSequenceEqual(actual, expected)
