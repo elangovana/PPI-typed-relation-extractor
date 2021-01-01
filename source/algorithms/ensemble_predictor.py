b001e018838ed2282263a4af0c062f0c57ea0fe4
aeg <aeg@amazon.com>
2021-01-01 13:02:50 +1100

    Try to use all gpus for ensemble inference

--- source/algorithms/ensemble_predictor.py
+++ source/algorithms/ensemble_predictor.py
@@ -1,4 +1,8 @@
+import logging
+from multiprocessing.dummy import Pool
 from operator import add
+
+import torch
 
 from algorithms.Predictor import Predictor
 
@@ -12,16 +16,27 @@ class EnsemblePredictor:
         if not self._is_iterable(model_networks):
             model_networks = [model_networks]
 
+        if device is None:
+            if torch.cuda.device_count() > 0:
+                devices = ["cuda:{}".format(i) for i in range(torch.cuda.device_count())]
+            else:
+                devices = ["cpu"]
+        else:
+            devices = [device]
+
+        self._logger.info("Using devices {}".format(devices))
+        model_device_map = [(m, dataloader, devices[i % len(devices)]) for i, m in enumerate(model_networks)]
+        with Pool(len(devices)) as p:
+            agg_pred_scores = p.starmap(self.model_wrapper.predict, model_device_map)
+
         scores_ensemble = []
-        for model_network in model_networks:
-            predicted, scores = self.model_wrapper.predict(model_network, dataloader, device)
-            self._populate_aggregate_scores_(scores, scores_ensemble)
+        for _, s in agg_pred_scores:
+            self._populate_aggregate_scores_(s, scores_ensemble)
 
         scores_ensemble_avg = []
         # average the confidence
         for batch in scores_ensemble:
             scores_ensemble_avg.append([list(map(lambda x: x / len(model_networks), p)) for p in batch])
-
 
         # Predicted ensemble , arg max
         predicted_ensemble = []
@@ -42,6 +57,9 @@ class EnsemblePredictor:
                     batch_sum.append(list(map(add, output_scores_ensemble[bi][line], batches_of_scores[bi][line])))
                 output_scores_ensemble[bi] = batch_sum
 
+    @property
+    def _logger(self):
+        return logging.getLogger(__name__)
 
     @staticmethod
     def _is_iterable(o):
