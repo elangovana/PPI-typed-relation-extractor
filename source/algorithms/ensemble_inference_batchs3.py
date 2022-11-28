da6e696c922c2dbf0b92d3ac165b676a449245e9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 18:52:51 +0000

    Adding tarfile member sanitization to extractall()

--- source/algorithms/ensemble_inference_batchs3.py
+++ source/algorithms/ensemble_inference_batchs3.py
@@ -45,7 +45,26 @@ class EnsembleInferenceBatchS3:
 
     def _extract_tar(self, tar_gz_file, dest_dir):
         with  tarfile.open(tar_gz_file) as tf:
-            tf.extractall(dest_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tf, dest_dir)
 
     def __call__(self, remote_model_path_list, local_dir=None):
 
